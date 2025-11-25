c6e89054a265cc1dfe954b7b3fe47aa4b3548123
Ahmed Amin <ahmedamin1700@gmail.com>
2025-11-25 04:49:16 +0200

    Fix run a program failure

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ def main():
                 result = subprocess.run(parts, capture_output=True, text=True, check=True)
                 print(f"Program was passed {len(parts)} args (including program name).")
                 for i, arg in enumerate(parts):
-                    print(f"Arg #{i} {"(program name)" if i == 0 else ""}: {arg}")
+                    print(f"Arg #{i}{":" if i != 0 else ""}{" (program name):" if i == 0 else ""} {arg}")
                 print(result.stdout)
             else:
                 print(f"{command}: command not found")
