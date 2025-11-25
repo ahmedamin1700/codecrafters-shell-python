a854288c295d6c3b3efbd2bae7a58e91e84d8fab
Ahmed Amin <ahmedamin1700@gmail.com>
2025-11-25 03:13:25 +0200

    Fix test failure

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
     command = input("")
-    sys.stdout.write(f"{command}: command not found.\n")
+    sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
